8ea3557cdf59f69534e7724f010d974ac8abb4f7
Michael Allen Smith <digitalcolony@gmail.com>
2025-02-02 11:16:21 -0800

    again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
 	redirects: {
-		"/pressure": "http://www.atmos.washington.edu/~neallog/temp_real_pressure.html",
+		"/pressure": {
+			status: 301,
+			destination: "http://www.atmos.washington.edu/~neallog/temp_real_pressure.html",
+		},
 	},
 });

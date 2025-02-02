77e140318e6045d2f78aaf1e8bf96df3deaa3f67
Michael Allen Smith <digitalcolony@gmail.com>
2025-02-02 11:31:02 -0800

    please work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
-	redirects: {
-		"/pressure": {
-			status: 301,
-			destination: "http://www.atmos.washington.edu/~neallog/temp_real_pressure.html",
-		},
-	},
+	adapter: netlify(),
 });
